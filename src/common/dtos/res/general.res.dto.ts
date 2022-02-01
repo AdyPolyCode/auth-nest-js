192768d7cd3000705dfef555efb4ba4151fc6a41
Ady <polcsek.dm90@gmail.com>
2022-02-01 19:20:11 +0100

    chore: add swagger properties

--- src/common/dtos/res/general.res.dto.ts
+++ src/common/dtos/res/general.res.dto.ts
@@ -1,9 +1,30 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export default class GeneralResDto<I> {
+    @ApiProperty({
+        type: String,
+        required: true,
+        description: 'response message depends on success or false',
+    })
     message: string;
 
+    @ApiProperty({
+        type: Number,
+        required: true,
+        description: 'response code depends on success or false',
+    })
     statusCode: number;
 
+    @ApiPropertyOptional({
+        type: Object,
+        description: 'response data',
+    })
     data?: I;
 
+    @ApiPropertyOptional({
+        type: Number,
+        description:
+            'response page of successfully fetched x number of items (GET)',
+    })
     pageNumber?: number;
 }
